c0b020f3d233709c0779f3195cce12466e803be0
Tim-Bender <57515383+Tim-Bender@users.noreply.github.com>
2021-03-12 03:44:46 -0800

    Update

--- main.py
+++ main.py
@@ -15,19 +15,32 @@ GREEN_LASER = pygame.image.load(os.path.join("images", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("images", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("images", "pixel_laser_yellow.png"))
 
-BG = pygame.image.load(os.path.join("images", "background-black.png"))
-
 WIDTH, HEIGHT = 750, 750
+BG = pygame.transform.scale(pygame.image.load(os.path.join("images", "background-black.png")), (WIDTH, HEIGHT))
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Invaders")
+pygame.display.set_icon(YELLOW_SPACE_SHIP)
 
 
 def main():
     run = True
     fps = 60
+    level = 1
+    lives = 5
+
     clock = pygame.time.Clock()
+
+    def redraw():
+        WIN.blit(BG, (0, 0))
+        pygame.display.update()
+
     while run:
         clock.tick(fps)
+        redraw()
         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
 
+main()
